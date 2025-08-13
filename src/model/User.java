6a7b96e0ce12f41c1fdda98f0d3968584feab45c
Ricardo Martínez <ricardo300302@gmail.com>
2025-08-13 09:49:13 -0500

    UserController class removed in order to change system logic (for now)

--- src/model/User.java
+++ src/model/User.java
@@ -1,63 +1,129 @@
 package model;
 
+import java.util.Scanner;
+
 public class User {
+    private static User[] users = new User[100];
     private String name;
     private static Integer id_generator = 0;
     private final Integer USER_ID;
-    private final String userName;
+    private final String USER_NAME;
     private String password;
-    private static User[] users = new User[100];
     private static Integer userIndex = 0;
 
     public User(String name, String password) {
+        users[userIndex] = this;
+        userIndex++;
         this.name = name;
         id_generator++;
         this.USER_ID = id_generator;
-        this.userName = name + USER_ID;
+        this.USER_NAME = name + USER_ID;
         this.password = password;
-    }
-
-    public static User[] getUsers() {
-        return users;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return password;
     }
 
     public Integer getUSER_ID() {
         return USER_ID;
     }
 
-    public static Integer getUserIndex() {
-        return userIndex;
+    private String getUserName() {
+        return USER_NAME;
     }
 
-    public static void increaseUserIndex() {
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void addUser(String name, String password){
+        users[userIndex] = new User(name, password);
         userIndex++;
     }
 
-    public static void decreaseUserIndex(){
-        userIndex--;
+    public User findUserById(Integer userId){
+
+        for(int i=0; i<users.length ;i++){
+            if(users[i] != null){
+                if(users[i].getUSER_ID() == userId){
+                    return users[i];
+                }
+            }
+        }
+
+        System.out.println("El usuario no se encuentra registrado");
+        return null;
     }
 
-    public static void setUsers(User[] users) {
-        User.users = users;
+    public User findUserByUserName(String userName){
+
+        for(int i=0; i<users.length ;i++){
+            if(users[i] != null){
+                if(users[i].getUserName().equals(userName)){
+                    return users[i];
+                }
+            }
+
+        }
+
+        System.out.println("El usuario no se encuentra registrado");
+        return null;
     }
+
+    public void setPassword(){
+        Scanner input = new Scanner(System.in);
+        Integer passwordCounter = 0;
+
+        do{
+            System.out.print("escriba la contraseña actual: ");         
+            String oldPassword = input.nextLine();
+
+            if(this.password.equals(oldPassword)){
+                System.out.println();
+                System.out.print("escriba la nueva contraseña: ");
+                this.password = input.nextLine();
+                System.out.println("\ncontraseña reestablecida");
+                input.close();
+                return;
+            }else{
+                passwordCounter++;
+                System.out.println("contraseña invalida\n");
+            }
+        } while(passwordCounter<3);
+
+        input.close();
+        System.out.println("ha superado el numero de intentos permitidos para ingresar su password");
+    }
+
+    public void deleteUser(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("ingrese el nombre de usuario a eliminar: ");
+        String userName = input.nextLine();
+        input.close();
+        User[] users = User.getUsers();
+
+        for(int i=0; i<users.length ;i++){
+            if(users[i] != null && users[i].getUserName().equals(userName)){
+
+                User[] newArray = new User[100];
+                for(int j=0; j<i ;j++) {
+                    newArray[j] = users[j];
+                }
+                for(int j=i; j<users.length-1 ;j++) {
+                    newArray[j] = users[j+1];
+                }
+
+                this.users = newArray;
+                this.userIndex--;
+                System.out.println("\nusuario eliminado exitosamente");
+                return;
+            }
+        }
+
+        System.out.println("\nel usuario no se encuentra registrado en el sistema");
+    }
+    
+    //TODO: eliminar getUsers
+    public static User[] getUsers() {
+        return users;
+    }
+
+
 }
--- src/view/App.java
+++ src/view/App.java
@@ -1,26 +1,18 @@
 package view;
-import controller.UserController;
-//TODO: no permitir a App acceder a User ???
-//TODO: encontrar usuario solo mostrar datos y no retornar User?
 import model.User;
 
 public class App {
     public static void main(String[] args) {
-        UserController u = new UserController();
-        u.addUser("ramon","1234567");
-        u.addUser("josefino", "7654321");
-        u.addUser("josefino3", "7654321");
+        User u1 = new User("carlo", "1234567");
+        User u2 = new User("ramiro", "7654321");
+        u2.addUser("josefino", "micontrasena");
 
-        u.removeUser();
-        User[] us = User.getUsers();
-
-        for(User i: us){
-            if(i != null){
-                System.out.println(i.getName() + " " + i.getUserName());
-            }else{
-                System.out.println(i);
+        u1.deleteUser();
+        
+        for(User u: User.getUsers()){
+            if(u != null){
+                System.out.println(u.getUSER_ID());
             }
-            
         }
     }
 }
